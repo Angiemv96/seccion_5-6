422c7c8ec909052b02cb382b7f4d9357282628f5
Angiemv96 <149022829+Angiemv96@users.noreply.github.com>
2023-12-06 12:55:01 -0500

    seccion_6

---
+++ quinto.py
@@ -0,0 +1,9 @@
+"""
+ Cree un programa que pregunte al usuario si desea salir, si o no “S/N”, si el usuario teclea la letra S el
+programa se detendrá, de lo contrario continuará ejecutándose.
+
+"""
+
+bandera = input("Ingrese s/n")
+while(bandera != "s"):
+    print ("gracias por tu tiempo")
